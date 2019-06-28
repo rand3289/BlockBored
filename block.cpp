cc2f9f9881caac6619b308b78cc21a038961c2dd
rand3289 <rand3289@rand3289.com>
2019-06-28 00:18:53 -0400

    colorful blocks

--- block.cpp
+++ block.cpp
@@ -12,19 +12,39 @@ extern void pstr(SDL_Renderer* renderer, int x, int y, const std::string& str); 
 class Missle{
     constexpr static double MISSLE_SPEED = 0.01;
     bool dead = false;
-    double x, y, angle;
+    double fx, fy, angle;
 public:
-    Missle(double X, double Y, double angleRad): x(X), y(Y), angle(angleRad) { }
+    Missle(double X, double Y, double angleRad): fx(X), fy(Y), angle(angleRad) { }
     bool isDead(){ return dead; }
-    void move(){ 
-	x += MISSLE_SPEED * cos(angle);
-        y += MISSLE_SPEED * sin(angle);
-        if(x < -1.0 || x > 1.0 || y < -1.0 || y > 1.0){ dead = true; }
-    }
     void draw(SDL_Renderer* rend, SDL_DisplayMode& dm){
-        int px = x*dm.w/2+dm.w/2;
-        int py = y*dm.h/2+dm.h/2;
+	fx += MISSLE_SPEED * cos(angle);
+        fy += MISSLE_SPEED * sin(angle);
+        if(fx < -1.0 || fx > 1.0 || fy < -1.0 || fy > 1.0){ dead = true; }
+
+        int px = fx*dm.w/2+dm.w/2;
+        int py = fy*dm.h/2+dm.h/2;
         SDL_RenderDrawPoint(rend, px, py);
+    }
+};
+
+
+class Block: public SDL_Rect{
+    double fx, fy, angle; // SPEED
+    unsigned char r,g,b;
+public:
+    Block(double X, double Y): fx(X), fy(Y) {
+        r = rand()%200+50;
+        g = rand()%200+50;
+        b = rand()%200+50;
+        w = 10;
+        h = 10;
+    }
+
+    void draw(SDL_Renderer* rend, SDL_DisplayMode& dm){ 
+        SDL_SetRenderDrawColor(rend, r, g, b, SDL_ALPHA_OPAQUE);
+        x = fx*dm.w/2+dm.w/2;
+        y = fy*dm.h/2+dm.h/2;
+        SDL_RenderFillRect(rend, this);
     }
 };
 
@@ -38,31 +58,44 @@ class Game {
     constexpr static double SHIP_SPEED = 0.003;
     double x = 0.0;
     double y = 0.0;
-//    std::vector<Block> blocks;
+    std::vector<Block> blocks;
     std::vector<Missle> missles;
 public:
+    void init();
+    Game(){ init(); }
     void shoot(){ fire = true; }
     void move(double angleRad, bool leftBtn, bool rightBtn){ angle = angleRad; left = leftBtn; right = rightBtn; }
     void draw(SDL_Renderer* rend, SDL_DisplayMode& dm);
 };
 
 
+void Game::init(){
+    for(int i =0; i < 50; ++i){
+        blocks.emplace_back( (rand()%1000-500)/500.0, (rand()%1000-500)/500.0 );
+    }
+}
+
+
 void Game::draw(SDL_Renderer* rend, SDL_DisplayMode& dm){
-    // run+draw missles:
-    SDL_SetRenderDrawColor(rend, 0xFF, 0x00, 0x00, SDL_ALPHA_OPAQUE);
+    // draw blocks:
+    for(Block& b: blocks){
+        b.draw(rend, dm);
+    }
+
+    // draw missles:
     missles.erase(std::remove_if(missles.begin(),missles.end(), [](Missle& m){ return m.isDead(); }), missles.end() );
 
     if(fire){
         fire = false;
-        missles.emplace_back(x, y, angle);
+        missles.emplace_back(x, y, angle); // current x,y,angle of the ship
     }
 
+    SDL_SetRenderDrawColor(rend, 0xFF, 0x00, 0x00, SDL_ALPHA_OPAQUE); // missles are all same color
     for(Missle& m: missles){
-        m.move();
         m.draw(rend, dm);
     }
 
-    // run + draw the ship:
+    // draw the ship:
     SDL_SetRenderDrawColor(rend, 0x00, 0xFF, 0x00, SDL_ALPHA_OPAQUE);
     double s = left  ?  SHIP_SPEED : 0;
            s = right ? -SHIP_SPEED : s;
@@ -77,9 +110,9 @@ void Game::draw(SDL_Renderer* rend, SDL_DisplayMode& dm){
     SDL_RenderDrawPoint(rend, px+1, py+1);
     SDL_RenderDrawPoint(rend, px-1, py+1);
     SDL_RenderDrawPoint(rend, px+1, py-1);
-     
+
     // draw some debugging stuff:
-    SDL_SetRenderDrawColor(rend, 0x00, 0x00, 0xFF, SDL_ALPHA_OPAQUE);
+    SDL_SetRenderDrawColor(rend, 0xFF, 0x00, 0x00, SDL_ALPHA_OPAQUE);
     int vx = 30.0 * cos(angle);
     int vy = 30.0 * sin(angle);
     SDL_RenderDrawLine(rend, 50, 50, vx+50, vy+50);
