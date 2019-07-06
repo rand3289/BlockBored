07cd0a60dea139e9ec43359183168f858b70747c
rand3289 <rand3289@rand3289.com>
2019-07-06 01:29:29 -0400

    tweaking audio

--- sdlaudio.cpp
+++ sdlaudio.cpp
@@ -2,17 +2,25 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <algorithm> // remove_if()
+using namespace std;
 
+class Audio;
 
 class AudioData {
-    double balance; // [0.0 through 1.0]
-    size_t pos;
-    size_t len;
+    friend Audio;
+    float balance; // [0.0 through 1.0]
+    Uint32 pos;
+
+    SDL_AudioSpec wav_spec;
+    Uint32 wav_length;
+    Uint8 *wav_buffer; // shared_ptr<Uint8> buffer; would be nice
 public:
     AudioData(): balance(0.5) {}
     AudioData(double balancE): balance(balancE) {}
+    ~AudioData(){ SDL_FreeWAV(wav_buffer); }
+    bool donePlaying() const { return pos == wav_length; }
     void load(const std::string& fileName);
-    bool donePlaying(){ return 0==len; }
 };
 
 
@@ -22,26 +30,30 @@ class Audio{
     std::vector<std::shared_ptr<AudioData> > audioData;
     void init();
 public:
-    Audio(): dev(0) { init(); }
+    Audio(){ init(); }
     ~Audio(){ SDL_CloseAudioDevice(dev); }
-    void mix(Uint8 *stream, int len);
+    void mix(Uint8 *stream, int len); // callback
     void play(const std::shared_ptr<AudioData>& data);
 };
 
 
-void audioCallback(void *userdata, Uint8 *stream, int len){
-    static_cast<Audio*>(userdata)->mix(stream, len);
+void AudioData::load(const std::string& fileName){
+    if (SDL_LoadWAV(fileName.c_str(), &wav_spec, &wav_buffer, &wav_length) == NULL) {
+        fprintf(stderr, "Could not open test.wav: %s\n", SDL_GetError());
+        wav_length = 0;
+        pos = 0;
+    }
 }
 
 
 void Audio::init(){
     want.freq = 22000;
     want.format = AUDIO_U8; // AUDIO_S8, AUDIO_S16LSB, AUDIO_U16LSB
-    want.channels = 2;
+    want.channels = 1;
     want.samples = 4096;
-    want.callback = [](void* userdata, Uint8 *stream, int len) { static_cast<Audio*>(userdata)->mix(stream, len); };
-//    want.callback = audioCallback; // TODO: use lambda instead???
     want.userdata = this;
+    want.callback = [](void* userdata, Uint8 *stream, int len) { static_cast<Audio*>(userdata)->mix(stream, len); };
+
     dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
     if ( dev < 0 ){
         fprintf(stderr, "Can't open audio: %s\n", SDL_GetError());
@@ -53,18 +65,13 @@ void Audio::init(){
 
 void Audio::mix(Uint8 *stream, int len){
     for(std::shared_ptr<AudioData>& data: audioData){
-//        SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);
+        Uint32 len2 = std::min((Uint32)len, data->wav_length - data->pos);
+        SDL_MixAudioFormat(stream, data->wav_buffer+data->pos, want.format, len2, SDL_MIX_MAXVOLUME);
+        data->pos+=len2;
     }
-    // TODO: delete what's donePlaying() here
+    auto end = std::remove_if(audioData.begin(), audioData.end(), [](shared_ptr<AudioData>& ad){ return ad->donePlaying(); } );
+    audioData.erase(end, audioData.end()); // delete what's donePlaying()
 }
-
-
-//class AudioLock(){
-//    SDL_AudioDeviceID dev;
-//public:
-//    AudioLock(SDL_AudioDeviceID device): dev(device) { SDL_LockAudioDevice(dev); }
-//    ~AudioLock(){ SDL_UnlockAudioDevice(dev); }
-//};
 
 
 void Audio::play(const std::shared_ptr<AudioData>& data){
@@ -72,3 +79,13 @@ void Audio::play(const std::shared_ptr<AudioData>& data){
     audioData.push_back(data); // this data will be mixed in during the next callback
     SDL_UnlockAudioDevice(dev);
 }
+
+
+/*
+class AudioLock {
+    SDL_AudioDeviceID dev;
+public:
+    AudioLock(SDL_AudioDeviceID device): dev(device) { SDL_LockAudioDevice(dev); }
+    ~AudioLock(){ SDL_UnlockAudioDevice(dev); }
+};
+*/
